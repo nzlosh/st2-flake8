3515ffd51a62677a860e64bf1f0a903a6b8467e9
W Chan <m4d.coder@gmail.com>
2019-05-17 21:24:03 +0000

    Add plugin to check apache and proprietary license header
    Add plugin and related unit tests to check apache and proprietary license header. Enable the license check extension for this repo.

---
+++ st2flake8/tests/fixtures/license_check/module_with_proprietary_license.py
@@ -0,0 +1,9 @@
+# Unauthorized copying of this file, via any medium is strictly
+# prohibited. Proprietary and confidential. See the LICENSE file
+# included with this work for details.
+
+from __future__ import absolute_import
+
+
+def MockClass(object):
+    pass
